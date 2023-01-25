f66d005565bb3465a76eac5523b25166ae6fe55f
Thiago Frank <luffyzao1000@gmail.com>
2023-01-25 11:24:54 -0300

    Teste no exercicio

--- exercises/js/variaveis com let/let.js
+++ exercises/js/variaveis com let/let.js
@@ -4,3 +4,5 @@ console.log(nomeDoUsuario, 'Seja bem-vindo!');
 console.log('Soube que você é novo por aqui', nomeDoUsuario);
 
 window.alert(nomeDoUsuario, 'é você?');
+
+//Primeiro exercicio em Javascript
