8ab4abc021637e8114499d8bbaaca4225b01aa18
Thiago Frank <luffyzao1000@gmail.com>
2023-01-25 11:20:51 -0300

    Create let.js

---
+++ exercises/js/variaveis com let/let.js
@@ -0,0 +1,6 @@
+let nomeDoUsuario = ('Mario');
+
+console.log(nomeDoUsuario, 'Seja bem-vindo!');
+console.log('Soube que você é novo por aqui', nomeDoUsuario);
+
+window.alert(nomeDoUsuario, 'é você?');
