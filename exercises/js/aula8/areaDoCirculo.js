a63d65636c82f791146ac673e2532199ec77e2e1
Thiago Frank <luffyzao1000@gmail.com>
2023-02-02 16:35:30 -0300

    more exercises

---
+++ exercises/js/aula8/areaDoCirculo.js
@@ -0,0 +1,7 @@
+let area;
+const pi = 3.14;
+const raio = 2.00;
+
+area = pi*(raio*raio);
+
+console.log(area);
---
+++ exercises/js/aula8/media.js
@@ -0,0 +1,10 @@
+const nota1 = 5.0;
+const nota2 = 7.1;
+const nota3 = 6.5;
+let media;
+let mediaFinal;
+
+media = (nota1)+(nota2)+(nota3)/3;
+mediaFinal = media/2;
+
+console.log(mediaFinal);
