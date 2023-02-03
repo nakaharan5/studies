9dbf7e324f68ed4ba09f311d3da0838256b08681
Thiago Frank <luffyzao1000@gmail.com>
2023-02-03 20:42:53 -0300

    primitive types study

---
+++ exercises/js/aula9/primitiveTypes.js
@@ -0,0 +1,14 @@
+const nome1 = 'thiago'; //Lines 1, 3 and 3 are strings
+const nome2 = "thiago";
+const nome3 =  `thiago`;
+
+const num1 = 2; // Number int
+const num2 = 2.3; // Number float
+
+const aprovado = true; // Boolean
+
+let nomeDoAluno; //Undefined
+
+let nomeDoProfessor = null; //This value does not exist for JS, Object is a bug.
+
+console.log(typeof(nomeDoProfessor));
