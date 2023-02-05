de41f0dcfe3e00385707ea1c602232c1f72287f2
Thiago Frank <luffyzao1000@gmail.com>
2023-02-05 13:26:06 -0300

    math expressions

--- exercises/js/aula11/prompts.js
+++ exercises/js/aula11/prompts.js
@@ -2,5 +2,18 @@ let num1 = prompt('Digite um numero');
 let num2 = prompt('Digite outro numero');
 num1 = parseInt(num1);
 num2 = parseInt(num2);
-let resultado = alert(num1+num2);
+let resultado = num1+num2;
+let resultado2 = num1-num2;
+let resultado3 = num1/num2;
+let resultado4 = num1*num2;
+let resultado5 = num1**num2;
+
+window.alert(`O resultado da adição foi: ${resultado}`);
+window.alert(`O resultado da subtração foi: ${resultado2}`);
+window.alert(`O resultado da divisão foi: ${resultado3}`);
+window.alert(`O resultado da multiplicação foi: ${resultado4}`);
+window.alert(`O resultado da potenciação foi: ${resultado5}`);
+
+
+
 
