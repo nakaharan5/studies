0c76b4ac559458ac45c9818bc813bdef9db8ff3f
Thiago Frank <thiagofrankas@gmail.com>
2024-06-08 17:14:51 -0300

    Nova feat

--- exercises/js/mathMax/sortear.js
+++ exercises/js/mathMax/sortear.js
@@ -1,4 +1,10 @@
 let lista = Array.from({length: 100}, ()=>Math.floor(Math.random()*100)+1);
 
 
-console.log(lista);
+//console.log(lista);
+
+
+
+let numero = Math.floor(Math.random());
+
+console.log("O numero gerado foi:", numero);
